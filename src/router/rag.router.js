742e47cd924abf15661c10fe4d39cf75bfe81576
thanhptks212k4 <phamtienthanh21022004@gmail.com>
2026-04-17 13:53:43 +0700

    fix chunk rag

--- src/router/rag.router.js
+++ src/router/rag.router.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const ragController = require('../controllers/rag.controller');
 const {verifyToken} = require("../middlewares/authAPI.middleware"); 
 const upload = require("../config/multer.config")
+const ragController = require('../controllers/client/rag.controller');  // dùng client controller đã có groupId + base
 
-
-router.post('/uploadfile', verifyToken ,upload.array('files', 10) ,ragController.uploadfile);
+router.post('/uploadfile', verifyToken, upload.array('files', 10), ragController.uploadfile);
 
 module.exports = router;
